44b84e5bccaa4b7207e6bf673db7027e400230de
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:42:51 +0200

    Adding code examples/xing_curl.sh

---
+++ code examples/xing_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.xing.com/jobs/search?benefit=1.795d28"}'
